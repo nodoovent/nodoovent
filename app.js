20e759943c2b3c21bd43cc24dd25385ebd5f3aa1
Maxime Journaux <journaux.maxime@gmail.com>
2013-12-06 22:50:58 +0100

    add callback on app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var http = require ( "http" );
 var Nodoovent = require ( "./nodoovent" );
 
-var nodoovent = new Nodoovent ( );
-var app = nodoovent.app;
+var nodoovent = new Nodoovent ( function ( ) {
 
-http.createServer ( app ).listen ( app.get ( "port" ), function ( ) {
-  console.log ( "Nodoovent API run on port " + app.get ( "port" ) + " with " + app.get ( "env" ) + " mode" );
+	var app = nodoovent.app;
+
+	http.createServer ( app ).listen ( app.get ( "port" ), function ( ) {
+	  console.log ( "Nodoovent API run on port " + app.get ( "port" ) + " with " + app.get ( "env" ) + " mode" );
+	} );
+
 } );
