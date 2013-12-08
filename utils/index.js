ce7fa1b2fbc8c453ac4574abc54a77edd6d7def9
Maxime Journaux <journaux.maxime@gmail.com>
2013-12-08 20:38:35 +0100

    add an email address validator

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
 module.exports = {
 
 	uid: require ( "./uid" ),
-	QueryChainer: require ( "./jugglingdb-querychainer" )
+	QueryChainer: require ( "./jugglingdb-querychainer" ),
+
+	validateEmail: function ( mail ) {
+		return new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i').test ( mail );
+	}
 
 }
