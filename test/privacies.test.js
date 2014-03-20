f75be33b85f1657cb349455af4621e535f1ea87e
Maxime Journaux <journaux.maxime@gmail.com>
2014-03-20 21:04:14 +0100

    delete mocha.opts + update status and privacies test (404 on DELETE PUT POST)

--- test/privacies.test.js
+++ test/privacies.test.js
@@ -11,50 +11,96 @@ module.exports = function ( nodoovent, url ) {
 
 	describe ( "Test /privacies end points:", function ( ) {
 
-		it ( "should have /privacies end point", function ( callback ) {
-			var req = supertest ( url ).get ( "/privacies" ).send ( );
+		var supertest404 = function ( req, callback ) {
 			req.end ( function ( err, res ) {
 				if ( err ) return callback ( err );
-				res.should.have.status ( 200 );
-				res.should.be.json;
-				res.body.should.be.an.instanceof ( Array );
-				for ( var i = 0; i < lprivacies; i++ ) {
-					res.body[i].should.have.property ( "id", i + 1 );
-					res.body[i].should.have.property ( "name", privacies[i] );
-					res.body[i].should.not.have.property ( "createdAt" );
-					res.body[i].should.not.have.property ( "updatedAt" );
-				}
-				callback ( );
+				res.should.have.status ( 404 );
+				callback ( )
 			} );
-		} );
+		}
 
+		describe ( "GET end points", function ( ) {
 
-		// test all end points /privacies/id
-		var TestPrivaciesWithId = function ( i ) {
-			it ( "should have /privacies/" + ( i + 1 ) + " end point (" + privacies[i] + ")", function ( callback ) {
-				var req = supertest ( url ).get ( "/privacies/" + ( i + 1 ) ).send ( );
+			it ( "should have /privacies end point", function ( callback ) {
+				var req = supertest ( url ).get ( "/privacies" ).send ( );
 				req.end ( function ( err, res ) {
 					if ( err ) return callback ( err );
 					res.should.have.status ( 200 );
 					res.should.be.json;
-					res.body.should.have.property ( "id", i + 1 );
-					res.body.should.have.property ( "name", privacies[i] );
-					res.body.should.not.have.property ( "createdAt" );
-					res.body.should.not.have.property ( "updatedAt" );
+					res.body.should.be.an.instanceof ( Array );
+					for ( var i = 0; i < lprivacies; i++ ) {
+						res.body[i].should.have.property ( "id", i + 1 );
+						res.body[i].should.have.property ( "name", privacies[i] );
+						res.body[i].should.not.have.property ( "createdAt" );
+						res.body[i].should.not.have.property ( "updatedAt" );
+					}
 					callback ( );
 				} );
 			} );
-		}
 
-		for ( var i = 0; i < lprivacies; i++ ) new TestPrivaciesWithId ( i );
 
-		it ( "should not have /privacies/" + ( lprivacies + 1 ) + " end point and should return 404 http code", function ( callback ) {
-			var req = supertest ( url ).get ( "/privacies/" + ( lprivacies + 1 ) ).send ( );
-			req.end ( function ( err, res ) {
-				if ( err )  return callback ( err );
-				res.should.have.status ( 404 );
-				callback ( );
+			// test all end points /privacies/id
+			var TestPrivaciesWithId = function ( i ) {
+				it ( "should have /privacies/" + ( i + 1 ) + " end point (" + privacies[i] + ")", function ( callback ) {
+					var req = supertest ( url ).get ( "/privacies/" + ( i + 1 ) ).send ( );
+					req.end ( function ( err, res ) {
+						if ( err ) return callback ( err );
+						res.should.have.status ( 200 );
+						res.should.be.json;
+						res.body.should.have.property ( "id", i + 1 );
+						res.body.should.have.property ( "name", privacies[i] );
+						res.body.should.not.have.property ( "createdAt" );
+						res.body.should.not.have.property ( "updatedAt" );
+						callback ( );
+					} );
+				} );
+			}
+
+			for ( var i = 0; i < lprivacies; i++ ) new TestPrivaciesWithId ( i );
+
+			it ( "should not have /privacies/" + ( lprivacies + 1 ) + " end point and should return 404 http code", function ( callback ) {
+				var req = supertest ( url ).get ( "/privacies/" + ( lprivacies + 1 ) ).send ( );
+				req.end ( function ( err, res ) {
+					if ( err )  return callback ( err );
+					res.should.have.status ( 404 );
+					callback ( );
+				} );
 			} );
+
+		} );
+
+		describe ( "POST, PUT and DELETE endpoints", function ( ) {
+
+			it ( "POST /privacies", function ( callback ) {
+				req = supertest ( url ).post ( "/privacies" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "POST /privacies/1", function ( callback ) {
+				req = supertest ( url ).post ( "/privacies/1" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "PUT /privacies", function ( callback ) {
+				req = supertest ( url ).put ( "/privacies" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "PUT /privacies/1", function ( callback ) {
+				req = supertest ( url ).put ( "/privacies/1" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "DELETE /privacies", function ( callback ) {
+				req = supertest ( url ).del ( "/privacies" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "DELETE /privacies/1", function ( callback ) {
+				req = supertest ( url ).del ( "/privacies/1" );
+				supertest404 ( req, callback );
+			} );
+
 		} );
 
 	} );
--- test/status.test.js
+++ test/status.test.js
@@ -11,49 +11,90 @@ module.exports = function ( nodoovent, url ) {
 
 	describe ( "Test /status end points:", function ( ) {
 
-		it ( "should have /status end point", function ( callback ) {
-			var req = supertest ( url ).get ( "/status" ).send ( );
+		var supertest404 = function ( req, callback ) {
 			req.end ( function ( err, res ) {
 				if ( err ) return callback ( err );
-				res.should.have.status ( 200 );
-				res.should.be.json;
-				res.body.should.be.an.instanceof ( Array );
-				for ( var i = 0; i < lstatus; i++ ) {
-					res.body[i].should.have.property ( "id", i+1 );
-					res.body[i].should.have.property ( "name", status[i] );
-					res.body[i].should.not.have.property ( "createdAt" );
-					res.body[i].should.not.have.property ( "updatedAt" );
-				}
-				callback ( );
+				res.should.have.status ( 404 );
+				callback ( )
 			} );
-		} );
+		}
 
-		// test all end points /status/id
-		var TestStatusWithId = function ( i ) {
-			it ( "should have /status/" + ( i + 1 ) + " end point (" + status[i] + ")", function ( callback ) {
-				req = supertest ( url ).get ( "/status/" + ( i + 1 )  ).send ( );
+		describe ( "GET end points", function ( ) {
+			it ( "should have GET /status end point", function ( callback ) {
+				var req = supertest ( url ).get ( "/status" ).send ( );
 				req.end ( function ( err, res ) {
 					if ( err ) return callback ( err );
 					res.should.have.status ( 200 );
 					res.should.be.json;
-					res.body.should.have.property ( "id", i + 1 );
-					res.body.should.have.property ( "name", status[i] );
-					res.body.should.not.have.property ( "createdAt" );
-					res.body.should.not.have.property ( "updatedAt" );
+					res.body.should.be.an.instanceof ( Array );
+					for ( var i = 0; i < lstatus; i++ ) {
+						res.body[i].should.have.property ( "id", i+1 );
+						res.body[i].should.have.property ( "name", status[i] );
+						res.body[i].should.not.have.property ( "createdAt" );
+						res.body[i].should.not.have.property ( "updatedAt" );
+					}
 					callback ( );
 				} );
 			} );
-		}
 
-		for ( var i = 0; i < lstatus; i++ ) new TestStatusWithId ( i );
+			// test all end points /status/id
+			var TestStatusWithId = function ( i ) {
+				it ( "should have GET /status/" + ( i + 1 ) + " end point (" + status[i] + ")", function ( callback ) {
+					req = supertest ( url ).get ( "/status/" + ( i + 1 )  ).send ( );
+					req.end ( function ( err, res ) {
+						if ( err ) return callback ( err );
+						res.should.have.status ( 200 );
+						res.should.be.json;
+						res.body.should.have.property ( "id", i + 1 );
+						res.body.should.have.property ( "name", status[i] );
+						res.body.should.not.have.property ( "createdAt" );
+						res.body.should.not.have.property ( "updatedAt" );
+						callback ( );
+					} );
+				} );
+			}
 
-		it ( "should not have /status/" +  ( lstatus + 1 ) + " end point and should return 404 http code", function ( callback ) {
-			req = supertest ( url ).get ( "/status/" + ( lstatus + 1 ) ).send ( );
-			req.end ( function ( err, res ) {
-				if ( err ) return callback ( err );
-				res.should.have.status ( 404 );
-				callback ( );
+			for ( var i = 0; i < lstatus; i++ ) new TestStatusWithId ( i );
+
+			it ( "should not have GET /status/" +  ( lstatus + 1 ) + " end point and should return HTTP 404", function ( callback ) {
+				req = supertest ( url ).get ( "/status/" + ( lstatus + 1 ) ).send ( );
+				supertest404 ( req, callback );
 			} );
+
+		} );
+
+		describe ( "POST, PUT and DELETE endpoints", function ( ) {
+
+			it ( "POST /status", function ( callback ) {
+				req = supertest ( url ).post ( "/status" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "POST /status/1", function ( callback ) {
+				req = supertest ( url ).post ( "/status/1" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "PUT /status", function ( callback ) {
+				req = supertest ( url ).put ( "/status" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "PUT /status/1", function ( callback ) {
+				req = supertest ( url ).put ( "/status/1" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "DELETE /status", function ( callback ) {
+				req = supertest ( url ).del ( "/status" );
+				supertest404 ( req, callback );
+			} );
+
+			it ( "DELETE /status/1", function ( callback ) {
+				req = supertest ( url ).del ( "/status/1" );
+				supertest404 ( req, callback );
+			} );
+
 		} );
 
 	} );
